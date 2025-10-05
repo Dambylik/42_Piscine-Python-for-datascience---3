ab45f21f7cb2ae47ddd0faea2265bef7c60ab79f
dambylik_mac <dambylik@gmail.com>
2025-10-05 18:11:27 +0200

    ex01

--- ex01/S1E7.py
+++ ex01/S1E7.py
@@ -2,12 +2,38 @@ from S1E9 import Character
 
 
 class Baratheon(Character):
-    #your code here
+    """Representing the Baratheon family"""
+
+    def __init__(self, first_name: str, is_alive: bool = True):
+        """Constructor for Baratheon, inherits Character."""
+        super().__init__(first_name, is_alive)
+        self.family_name = "Baratheon"
+        self.eyes = "brown"
+        self.hairs = "dark"
+
+    def __str__(self):
+        return f"Vector: ({self.family_name}, {self.eyes}, {self.hairs})"
+
+    def __repr__(self):
+        return f"Vector: ({self.family_name}, {self.eyes}, {self.hairs})"
 
 
 class Lannister(Character):
-    #your code here
-    
-    # decorator
-    def create_lannister:
-        pass
+    """Representing the Lannister family"""
+
+    def __init__(self, first_name: str, is_alive: bool = True):
+        """Constructor for Lannister, inherits Character."""
+        super().__init__(first_name, is_alive)
+        self.family_name = "Lannister"
+        self.eyes = "blue"
+        self.hairs = "light"
+
+    def __str__(self):
+        return f"Vector: ({self.family_name}, {self.eyes}, {self.hairs})"
+
+    def __repr__(self):
+        return f"Vector: ({self.family_name}, {self.eyes}, {self.hairs})"
+
+    @classmethod
+    def create_lannister(cls, first_name, is_alive=True):
+        return cls(first_name, is_alive)
