bad30250956150b21c3b8d0ba96dbf8ea734e6da
dambylik_mac <dambylik@gmail.com>
2025-10-05 18:36:14 +0200

    ex03 wip

---
+++ ex03/ft_calculator.py
@@ -0,0 +1,14 @@
+class calculator:
+    def __init__(self):
+        pass
+    
+    def __add__(self, object) -> None:
+        
+        
+        
+        
+#         def __mul__(self, object) -> None:
+# #your code here
+# def __sub__(self, object) -> None:
+# #your code here
+# def __truediv__(self, object)
