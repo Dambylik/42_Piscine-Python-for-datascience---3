cde069f0498bca91aca0fe2364df31d6748e5a90
dambylik_mac <dambylik@gmail.com>
2025-10-19 14:44:19 +0200

    ex03

--- ex03/ft_calculator.py
+++ ex03/ft_calculator.py
@@ -1,14 +1,51 @@
 class calculator:
-    def __init__(self):
-        pass
-    
-    def __add__(self, object) -> None:
-        
-        
-        
-        
-#         def __mul__(self, object) -> None:
-# #your code here
-# def __sub__(self, object) -> None:
-# #your code here
-# def __truediv__(self, object)
+    """Calculator class for vector-scalar operations."""
+
+    def __init__(self, vector: list[float]):
+        """Initialize with a list of floats."""
+        self.vector = vector
+
+    def __add__(self, object: int | float) -> None:
+        """Add a scalar to each element of the vector."""
+        if isinstance(object, (int, float)):
+            new_list = []
+            for value in self.vector:
+                new_list.append(value + object)
+            self.vector = new_list
+            print(self.vector)
+        else:
+            print("Error: object must be an int or float")
+
+    def __mul__(self, object: int | float) -> None:
+        """Multiply each element of the vector by a scalar."""
+        if isinstance(object, (int, float)):
+            new_list = []
+            for value in self.vector:
+                new_list.append(value * object)
+            self.vector = new_list
+            print(self.vector)
+        else:
+            print("Error: object must be an int or float")
+
+    def __sub__(self, object) -> None:
+        """Subtract a scalar from each element of the vector."""
+        if isinstance(object, (int, float)):
+            new_list = []
+            for value in self.vector:
+                new_list.append(value - object)
+            self.vector = new_list
+            print(self.vector)
+        else:
+            print("Error: object must be an int or float")
+
+    def __truediv__(self, scalar: int | float) -> None:
+        """Divide each element of the vector by a scalar."""
+        if isinstance(scalar, (int, float)) and scalar != 0:
+            new_list = []
+            for value in self.vector:
+                result = value / scalar
+                new_list.append(result)
+            self.vector = new_list
+            print(self.vector)
+        else:
+            print("Error: operand must be a non-zero int or float")
